b6153f1a46b1177597001a6c335522dbf3dac72d
Isaac van Son <isaac.vanson@itvitaelearning.nl>
2025-04-09 22:30:47 +0200

    Add pytest to CI pipeline

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,13 @@
-import re
-
 import pytest
-
-from app.app import create_app
-
+from app import app
 
 @pytest.fixture
 def client():
-    app = create_app()
-    app.config["TESTING"] = True
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-
-def test_square(client):
-    answer = client.get("square_number?number=2")
-    assert b"Square of 2.0 is 4.0" == answer.data
-
-
-def test_hello_world(client):
-    answer = client.get("/")
-    assert re.match(b".* on host .*", answer.data)
+def test_square_number(client):
+    response = client.get('/square_number?number=2')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Square of 2.0 is 4.0"
